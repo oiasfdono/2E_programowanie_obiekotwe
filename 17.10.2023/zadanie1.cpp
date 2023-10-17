17c5d517bd8fa53f700c09468702bf8fcd60805b
oiasfdono <miernyjeses@gmail.com>
2023-10-17 15:00:42 +0200

    zadanie1 zadanie 2

---
+++ 17.10.2023/zadanie1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	const std::string prawidlowyLogin = "twój login";
+	const std::string prawidloweHaslo = "twoje haslo";
+	int iloscProb = 3;
+
+	while (iloscProb > 0)
+	{
+		std::string wprowadzonyLogin, wprowadzoneHaslo;
+
+		std::cout << "Podaj login: ";
+		std::cin >> wprowadzonyLogin;
+		std::cout << "Podaj haslo: ";
+		std::cin >> wprowadzoneHaslo;
+		if (wprowadzonyLogin == prawidlowyLogin && wprowadzoneHaslo == prawidloweHaslo)
+		{
+			std::cout << "Zalogowano pomyslnie" << std::endl;
+			break;
+		}
+		else
+		{
+			iloscProb--;
+			std::cout << "Błędny login lub hasło. Pozostało prób: " << iloscProb << std::endl;
+
+			if (iloscProb == 0)
+			{
+				std::cout << "Zablokowano możliwość logowania." << std::endl;
+			}
+		}
+	}
+	return 0;
+}
---
+++ 17.10.2023/zadanie2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	int liczba;
+	int iloczyn = 1;
+
+	std::cout << "Podaj liczby (0 kończy program): ";
+	while (true)
+	{
+		std::cin >> liczba;
+
+		if (liczba == 0)
+		{
+			break;
+		}
+		if (liczba % 2 == 0)
+		{
+			iloczyn *= liczba;
+		}
+		else
+
+		{
+			std::cout << "To nie jest liczba parzsta. Podaj inną liczbę: ";
+		}
+	}
+	std::cout << "Iloczyn liczb parzystych: " << iloczyn << std::endl;
+	return 0;
+}
